e2307e2bfe22cbcaf01d21e916d3e8ef4aa20789
saranshjaiswal426 <Co.2019.sdjaiswal@bitwardha.ac.in>
2023-02-25 15:57:28 +0530

    get unique element of a list

---
+++ get unique element.py
@@ -0,0 +1,6 @@
+restaurants = ["McDonald's", "Burger King", "McDonald's", "Chicken Chicken","Burger King"]
+unique_restaurants = set(restaurants)
+print(unique_restaurants)
+print(list(unique_restaurants))
+print(list(set(restaurants)))
+
